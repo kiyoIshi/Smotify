dd24a87e8580d9e10f6795a8a1a69532463df55a
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-05 14:46:18 +0900

    separate handlers

--- register.php
+++ register.php
@@ -1,46 +1,8 @@
 <?php
-
-function sanitizeFormPassword ($inputText) {
-  $inputText = strip_tags($inputText);
-  return inputText;
-}
-
-function sanitizeFormUsername ($inputText) {
-  $inputText = strip_tags($inputText);
-  $inputText = str_replace(" ", "", $inputText);
-  return inputText;
-}
-
-function sanitizeFormString ($inputText) {
-  $inputText = strip_tags($inputText);
-  $inputText = str_replace(" ", "", $inputText);
-  $inputText = ucfirst(strtolower($inputText));
-  return inputText;
-}
-
-if(isset($_POST['loginButton'])){
- //Login button was pressed"
-}
-
-if(isset($_POST['resisterButton'])){
-  //Register button was pressed"
-  $username = sanitizeFormUsername($_POST['username']);
-
-  $firsrname = sanitizeFormString($_POST['firstName']);
-
-  $lastName = sanitizeFormString($_POST['lastNameame']);
-
-  $email = sanitizeFormString($_POST['email']);
-
-  $email2 = sanitizeFormString($_POST['email2']);
-
-  $password = sanitizeFormPassword($_POST['password']);
-
-  $password2 = sanitizeFormPassword($_POST['password2']);
-
- }
-
+ include("include/handlers/register-handler.php");
+ include("include/handlers/login-handler.php");
 ?>
+
 <html lang="en">
 <head>
   <title>Welcome to Smotify!</title>

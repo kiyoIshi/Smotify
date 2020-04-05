c63a3d4df2fe650e4f4665bc0b3d63525debce71
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-05 14:29:19 +0900

    add funtcions to clean the input

--- register.php
+++ register.php
@@ -1,4 +1,46 @@
-<!DOCTYPE html>
+<?php
+
+function sanitizeFormPassword ($inputText) {
+  $inputText = strip_tags($inputText);
+  return inputText;
+}
+
+function sanitizeFormUsername ($inputText) {
+  $inputText = strip_tags($inputText);
+  $inputText = str_replace(" ", "", $inputText);
+  return inputText;
+}
+
+function sanitizeFormString ($inputText) {
+  $inputText = strip_tags($inputText);
+  $inputText = str_replace(" ", "", $inputText);
+  $inputText = ucfirst(strtolower($inputText));
+  return inputText;
+}
+
+if(isset($_POST['loginButton'])){
+ //Login button was pressed"
+}
+
+if(isset($_POST['resisterButton'])){
+  //Register button was pressed"
+  $username = sanitizeFormUsername($_POST['username']);
+
+  $firsrname = sanitizeFormString($_POST['firstName']);
+
+  $lastName = sanitizeFormString($_POST['lastNameame']);
+
+  $email = sanitizeFormString($_POST['email']);
+
+  $email2 = sanitizeFormString($_POST['email2']);
+
+  $password = sanitizeFormPassword($_POST['password']);
+
+  $password2 = sanitizeFormPassword($_POST['password2']);
+
+ }
+
+?>
 <html lang="en">
 <head>
   <title>Welcome to Smotify!</title>
@@ -63,4 +105,3 @@
   </div>
   
 </body>
-</html>

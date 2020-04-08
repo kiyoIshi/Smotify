662bf52f3e2ac84f68a484c3441515168012cb05
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-08 13:33:18 +0900

    create config.php

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+  ob_start();
+
+  $timezone = date_default_timezone_set("Asia/Tokyo");
+
+  $con = mysqli_connect("localhost", "root", "root", "smotify");
+
+  if(mysqli_connect_errno()) {
+    echo "Failed to connect: " .mysql_connect_errno();
+  }
+
+?>

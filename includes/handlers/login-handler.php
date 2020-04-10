b4646731e776c0d30f0e6d60d896ba2e202fcef7
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-10 19:37:06 +0900

    add sessions

--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -7,6 +7,7 @@
   $result = $account->login($username, $password);
 
   if ($result == true) {
+    $_SESSION['userLoggedIn'] = $username;
     header("Location: index.php");
   }
   }
--- index.php
+++ index.php
@@ -1,11 +1,20 @@
-<html lang="en">
+<?php
+include("includes/config.php");
+
+//session_destroy();
+
+if (isset($_SESSION['userLoggedIn'])) {
+  $userLoggedIn = $_SESSION['userLoggedIn'];
+}
+else {
+  header("Location: register.php");
+}
+?>
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Welcome to Smotify!</title>
 </head>
 <body>
-  
+  Hello!
 </body>
 </html>

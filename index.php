54f16dfb1004fbaddbcbc79d5561564a9bb7bc8f
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-12 16:04:36 +0900

    create groups in navBar

--- index.php
+++ index.php
@@ -27,7 +27,30 @@ else {
         <a href="index.php" class="logo">
           <img src="assets/images/icons/logo.png" alt="">
         </a>
+
+        <div class="group">
+          <div class="navItem">
+            <a href="search.php" class="navItemLink">Search</a>
+          </div>
+
+        </div>
+
+        <div class="group">
+          <div class="navItem">
+            <a href="browse.php" class="navItemLink">Browse</a>
+          </div>
+
+          <div class="navItem">
+            <a href="yourMusic.php" class="navItemLink">Your Music</a>
+          </div>
+
+          <div class="navItem">
+            <a href="pumpkinPie.php" class="navItemLink">Pumpkin Pie</a>
+          </div>
+          
+        </div>
       </nav>
+
     </div>
   </div>
 

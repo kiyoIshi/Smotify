b81734612f1441af72a610b8215466a493bf5f9b
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-14 12:20:46 +0900

    create Artist class

--- album.php
+++ album.php
@@ -10,13 +10,10 @@ else {
 $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
 $album = mysqli_fetch_array($albumQuery);
 
-$artistId = $album['artist'];
+$artist = new Artist($con, $album['artist']);
 
-$artistQuery = mysqli_query($con, "SELECT * FROM artists WHERE id='$artistId'");
-$artist = mysqli_fetch_array($artistQuery);
-
-echo $artist['name'] . "<br>";
-echo $album['title'];
+echo $album['title'] . "<br>";
+echo $artist->getName();
 
 ?>
 

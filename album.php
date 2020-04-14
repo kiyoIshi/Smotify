de63e36464ba52e86e7630fc51feaac615a30d5c
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-14 12:06:22 +0900

    show arstist name

--- album.php
+++ album.php
@@ -10,6 +10,12 @@ else {
 $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
 $album = mysqli_fetch_array($albumQuery);
 
+$artistId = $album['artist'];
+
+$artistQuery = mysqli_query($con, "SELECT * FROM artists WHERE id='$artistId'");
+$artist = mysqli_fetch_array($artistQuery);
+
+echo $artist['name'] . "<br>";
 echo $album['title'];
 
 ?>

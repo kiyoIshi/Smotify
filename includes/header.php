a62a6d95a1bab7296c9d2320356d26321e1111b6
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-16 14:15:45 +0900

    include jQuery

--- includes/header.php
+++ includes/header.php
@@ -17,7 +17,8 @@ else {
 <head>
   <title>Welcome to Smotify!</title>
   <link rel="stylesheet" href="assets/css/style.css">
-
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="assets/js/script.js"></script>
 </head>
 <body>

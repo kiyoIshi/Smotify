a7988b71289f9dfc8853749d9588e8e641f76839
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-16 19:16:58 +0900

    create getArtistJson.php

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -24,6 +24,16 @@ $jsonArray = json_encode($resultArray);
     $.post("includes/handlers/ajax/getSongJson.php", { songId: trackId }, function(data) {
 
       var track = JSON.parse(data);
+
+      $(".trackName span").text(track.title);
+
+      $.post("includes/handlers/ajax/getArtistJson.php", { artistId: track.artist }, function(data) {
+
+        var artist = JSON.parse(data);
+
+        $(".artistName span").text(artist.name);
+      });
+
       audioElement.setTrack(track.path);
       audioElement.play;
     })
@@ -61,11 +71,11 @@ $jsonArray = json_encode($resultArray);
       <div class="trackInfo">
 
         <span class="trackName">
-          <span>Hello Hello</span>
+          <span></span>
         </span>
 
         <span class="artistName">
-          <span>Pumpkin Pie</span>
+          <span></span>
         </span>
 
       </div>

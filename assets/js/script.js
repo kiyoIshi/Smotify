82953add33ea8ee272b73b424affb27bd55f3bf3
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-17 13:46:00 +0900

    display the remaining time

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,11 @@ function Audio() {
   this.currentlyPlaying;
   this.audio = document.createElement('audio');
 
+  this.audio.addEventListener("canplay", function() {
+    //'this' refers to the object that the event was called on
+    $(".progressTime.remaining").text(this.duration);
+  });
+
   this.setTrack = function(track) {
     this.currentlyPlaying = track;
     this.audio.src = track.path;

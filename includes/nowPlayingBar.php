08f1b74be6e7a9c410101b1fb6e3e6cb7988fc11
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-17 18:38:52 +0900

    controll the volume

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -33,6 +33,29 @@ $jsonArray = json_encode($resultArray);
       timeFromOffset(e, this);
     });
 
+    $(".volumeBar .progressBar").mousedown(function() {
+      mouseDown = true;
+    });
+
+    $(".volumeBar .progressBar").mousemove(function(e) {
+      if(mouseDown) {
+
+        var percentage = e.offsetX / $(this).width();
+
+        if(percentage >= 0 && percentage <= 1) {
+          audioElement.audio.volume = percentage;
+        }     
+      } 
+    });
+
+    $(".volumeBar .progressBar").mouseup(function(e) {
+      var percentage = e.offsetX / $(this).width();
+        
+      if(percentage >= 0 && percentage <= 1) {
+        audioElement.audio.volume = percentage;
+      } 
+    });
+
     $(document).mouseup(function() {
       mouseDown = false;
     })

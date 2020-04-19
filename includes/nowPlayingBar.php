e4fa0ff8f964e16cbef7f41a34edb3df1675e2a5
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-19 17:10:20 +0900

    create function shuffleArray

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -118,9 +118,34 @@ $jsonArray = json_encode($resultArray);
     shuffle = !shuffle;
     var imageName = shuffle ? "shuffle-active.png" : "shuffle.png";
     $(".controlButton.shuffle img").attr("src", "assets/images/icons/" + imageName);
+
+    if(shuffle == true) {
+      //Randomize playlist
+      shuffleArray(shufflePlaylist);
+    }
+    else {
+      //shuffle has been deactivated
+      //go back to regular playlist
+    }
+  }
+
+  function shuffleArray(a) {
+    var j, x, i;
+    for(i = a.length; i; i--) {
+      j = Math.floor(Math.random() * 1);
+      x = a[i -1];
+      a[i -1] = a[j];
+      a[j] = x;
+    }
   }
 
   function setTrack(trackId, newPlaylist, play) {
+
+    if(newPlaylist != currentPlaylist) {
+      currentPlaylist = newPlaylist;
+      shufflePlaylist = currentPlaylist.slice();
+      shuffleArray(shufflePlaylist);
+    }
 
     currentIndex = currentPlaylist.indexOf(trackId);
     pauseSong();

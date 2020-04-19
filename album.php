35b5fca0b237e78b4b564a12824566f1a821907c
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-19 20:11:18 +0900

    play songs on clicking the play button

--- album.php
+++ album.php
@@ -36,7 +36,7 @@ $artist = $album->getArtist();
 
           echo "<li class='trackListRow'>
                   <div class='trackCount'>
-                    <img class='play' src='assets/images/icons/play-white.png'>
+                    <img class='play' src='assets/images/icons/play-white.png' onclick='setTrack(" . $albumSong->getId() . ", tempPlaylist, true)'>
                     <span class='trackNumber'>$i</span>
                   </div>
 
@@ -58,6 +58,11 @@ $artist = $album->getArtist();
           $i = $i + 1;
         }
       ?>
+
+      <script>
+        var tempSongIds = '<?php echo json_encode($songIdArray); ?>';
+        tempPlaylist = JSON.parse(tempSongIds);
+      </script>
 
   </ul>
 </div>

0322d7054dd47cecfa79219b09fe66c06a2acd09
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-20 20:12:34 +0900

    add albums section to the artist page

--- artist.php
+++ artist.php
@@ -29,7 +29,8 @@ $artist = new Artist($con, $artistId);
 </div>
 
 <div class="trackListContainer borderBottom"">
-  <ul class="trackList">   
+  <h2>SONGS</h2>
+  <ul class="trackList"> 
 
     <?php
       $songIdArray = $artist->getSongsId();
@@ -75,4 +76,33 @@ $artist = new Artist($con, $artistId);
     </script>
 
   </ul>
+</div>
+
+<div class="gridViewContainer">
+
+  <h2>ALBUMS</h2>
+
+  <?php
+    $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE artist='$artistId'");
+
+    while($row = mysqli_fetch_array($albumQuery)) {
+      
+      echo "<div class='gridViewItem'>
+
+              <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
+
+                <img src='" . $row['artWorkPath'] . "'>
+
+                <div class='gridViewInfo'>"
+
+                  . $row['title'] .
+
+                "</div>
+              </span>
+
+            </div>";
+
+    }
+  ?>
+
 </div>

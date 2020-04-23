b99cfcd387641c4d97995cbb584cb6d51deeb94a
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-23 16:13:26 +0900

    show results for album search

--- search.php
+++ search.php
@@ -34,6 +34,8 @@ else {
 
 </script>
 
+<?php if($term == "") exit(); ?>
+
 <div class="trackListContainer borderBottom"">
   <h2>SONGS</h2>
   <ul class="trackList"> 
@@ -120,4 +122,36 @@ else {
 			</div>";
 	}
   ?>
+</div>
+<div class="gridViewContainer">
+
+  <h2>ALBUMS</h2>
+
+  <?php
+    $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE title LIKE '$term%' LIMIT 10");
+
+    if(mysqli_num_rows($albumQuery) == 0) {
+      echo "<span class='noResults'>No albums found matching " . $term . "</span>";
+    }
+
+    while($row = mysqli_fetch_array($albumQuery)) {
+      
+      echo "<div class='gridViewItem'>
+
+              <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
+
+                <img src='" . $row['artWorkPath'] . "'>
+
+                <div class='gridViewInfo'>"
+
+                  . $row['title'] .
+
+                "</div>
+              </span>
+
+            </div>";
+
+    }
+  ?>
+
 </div>

c9fe882a271ab1b5e5609135f489ad47f7b4200c
Kiyoka Ishikawa <kiyonfu@gmail.com>
2020-04-26 19:07:16 +0900

    add IFNULL

--- includes/handlers/ajax/addToPlaylist.php
+++ includes/handlers/ajax/addToPlaylist.php
@@ -5,15 +5,9 @@ if(isset($_POST['playlistId']) && isset($_POST['songId'])) {
   $playlistId = $_POST['playlistId'];
   $songId = $_POST['songId'];
 
-  $orderIdQuery = mysqli_query($con, "SELECT MAX(playlistOrder) + 1 as playlistOrder FROM playlistSongs WHERE playlistId='$playlistId'");
+  $orderIdQuery = mysqli_query($con, "SELECT IFNULL(MAX(playlistOrder) + 1, 1) as playlistOrder FROM playlistSongs WHERE playlistId='$playlistId'");
   $row = mysqli_fetch_array($orderIdQuery);
   $order = $row['playlistOrder'];
-
-  if($order != ""){
-    $order = $row['playlistOrder'];
-  }else{
-    $order = $row['playlistOrder']+1;
-  }
 
   $query = mysqli_query($con, "INSERT INTO playlistSongs VALUES(NULL, '$songId', '$playlistId', '$order')");
 }
